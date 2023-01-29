bd3337ccc738ae79015238bf42a289070c39a5fc
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-01-29 19:27:46 +0200

    add base exercises 2-5

---
+++ src/ua/ithillel/lesson3/BaseHomework/Main.java
@@ -0,0 +1,38 @@
+package ua.ithillel.lesson3.BaseHomework;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String source;
+        StringMethods test = new StringMethods();
+
+        // --- Задание 2: сколько раз встречается символ в строке-источнике
+        char ch = 'a';
+        source = "We are the champions";
+        int countSymbols = test.findSymbolOccurance(source, ch);
+        System.out.println("Источник - \"" + source + "\", символ '" + ch + "' встречается " + countSymbols + " раз");
+
+        // --- Задание 3: выясняем является подстрока target частью строки-источника
+        // --- 1-й вариант
+        source = "Apollo";
+        String target = "pollo";
+        int index = test.findWordPosition(source, target);
+        System.out.println("Источник - \"" + source + "\", подстрока \"" + target + "\", индекс в источнике = " + index);
+
+        // --- 2-й вариант
+        source = "Apple";
+        target = "plant";
+        index = test.findWordPosition(source, target);
+        System.out.println("Источник - \"" + source + "\", подстрока \"" + target + "\", индекс в источнике = " + index);
+
+        // --- Задание 4: передаем строку и возвращаем строку-реверс
+        source = "Hello";
+        String reverse = test.stringReverse(source);
+        System.out.println("Слово-источник - \"" + source + "\", реверс - \"" + reverse + "\"");
+
+        // --- Задание 5: передаем слово и проверяем является ли оно полиндромом
+        source = "заказ";
+        boolean isWordPalindrome = test.isPalindrome(source);
+        System.out.println("Слово-источник - \"" + source + "\", является ли полиндромом = " + isWordPalindrome);
+    }
+}
---
+++ src/ua/ithillel/lesson3/BaseHomework/StringMethods.java
@@ -0,0 +1,39 @@
+package ua.ithillel.lesson3.BaseHomework;
+
+public class StringMethods {
+
+    // --- Задание 2: сколько раз встречается символ в строке-источнике
+    public int findSymbolOccurance(String aString, char aChar){
+
+        int countSymbols = 0;
+        char[] array = aString.toCharArray();
+
+        for (char ch: array) {
+            if(ch == aChar) ++countSymbols;
+        }
+
+        return countSymbols;
+    }
+
+    // --- Задание 3: выясняем является подстрока target частью строки-источника
+    public int  findWordPosition(String source, String target){
+        return source.indexOf(target);
+    }
+
+    // --- Задание 4: передаем строку и возвращаем строку-реверс
+    public String stringReverse(String aString){
+
+        StringBuffer s = new StringBuffer(aString);
+        s.reverse();
+
+        return s.toString();
+    }
+
+    // --- Задание 5: передаем слово и проверяем является ли оно полиндромом
+    public  boolean isPalindrome(String aString){
+
+        String s = stringReverse(aString);
+
+        return  aString.equals(s);
+    }
+}
