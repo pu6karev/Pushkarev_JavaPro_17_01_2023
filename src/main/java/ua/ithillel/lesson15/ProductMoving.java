1cdb74287d84420ba3950e289fdc8c7b1ecd1d28
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-03-24 12:12:36 +0200

    add exercise 14

---
+++ src/main/java/ua/ithillel/lesson15/ProductMoving.java
@@ -0,0 +1,88 @@
+package ua.ithillel.lesson15;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ProductMoving {
+
+    private final List<Product> list = new ArrayList<>();
+
+    public void add(Product product){
+        list.add(product);
+    }
+
+    public List<Product> getWholeList(){
+        return list;
+    }
+
+    public List<Product> getCategoryForPrice(Category category, double minPrice) {
+
+        List<Product> categoryAndPrice = list.stream()
+                .filter(s -> s.getCategory() == category && s.getPrice() > minPrice)
+                .toList();
+
+        return categoryAndPrice;
+    }
+
+    public List<Product> getCategoryWithDiscount(Category category, double discountPercent) {
+
+        List<Product> categoryAndDiscount = list.stream()
+                .filter(s -> (s.getCategory() == category && s.isDiscount()))
+                .peek(s -> s.setDiscount(discountPercent))
+                .toList();
+
+        return categoryAndDiscount;
+    }
+
+    public Product getCheapest() {
+
+        Product minPriceProd = list.stream()
+                .min((s1,s2) -> Double.compare(s1.getPrice(), s2.getPrice()))
+                .orElseThrow(IllegalStateException::new);
+
+        return minPriceProd;
+    }
+
+    // --- получим список объектов с последними добавленными продуктами
+    public List<Product> getLatestProducts(int numbOfLastProducts) {
+
+        List<Product> listLatest = list.stream()
+                .sorted(Comparator.comparing(Product::getAddingDate))
+                .skip(list.size() - numbOfLastProducts)
+                .toList();
+
+        return listLatest;
+    }
+
+    // --- получим общую сумму продуктов, начиная с указанной даты, нужной категории и цене, не выше заданной
+    public double getSumByCriterion(LocalDate startDate, Category category, double maxPrice) {
+
+        double listSum = list.stream()
+                .filter(s -> s.getAddingDate().isAfter(startDate) && s.getCategory()==category && s.getPrice() <= maxPrice)
+                .mapToDouble(Product::getPrice)
+                .sum();
+
+        return listSum;
+    }
+
+
+    // --- группируем по категории и собираем в List соответсвующие объекты Product
+    public Map<Category, List<Product>> getMapList(){
+
+        Map<Category, List<Product>> map = list.stream().collect(Collectors.groupingBy(Product::getCategory));
+
+        return map;
+    }
+
+    // --- группируем по категории и собираем в строку соответсвующие названия продуктов через запятую
+    public Map<Category, String> getMapString(){
+
+        Map<Category, String> map = list.stream().collect(Collectors.groupingBy(Product::getCategory,
+                Collectors.mapping(Product::getName, Collectors.joining(", "))));
+
+        return map;
+    }
+
+
+}
---
+++ src/test/java/ua/ithillel/lesson15/ProductMovingTest.java
@@ -0,0 +1,113 @@
+package ua.ithillel.lesson15;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductMovingTest {
+    ProductMoving productMoving = new ProductMoving();
+    Product[] arrayProduct = new Product[6];
+
+    @BeforeEach
+    public void init(){
+        LocalDate date = LocalDate.of(2020,12,12);
+        arrayProduct[0] = new Product("OOP", Category.BOOK, 420, true, date);
+        arrayProduct[1] = new Product("Java", Category.BOOK, 699, false, date.plusYears(1));
+        arrayProduct[2] = new Product("Johnson", Category.PEN, 2.5, false, date);
+        arrayProduct[3] = new Product("Rainbow", Category.PAINT, 12, false, date.plusMonths(5));
+        arrayProduct[4] = new Product("Python", Category.BOOK, 190, true, date.minusYears(1));
+        arrayProduct[5] = new Product("Fortran", Category.BOOK, 38, true, date.minusYears(5));
+    }
+
+    @Test
+    void shouldAdd() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        //assertArrayEquals(arrayProduct, );
+        assertEquals(arrayProduct.length, productMoving.getWholeList().size());
+    }
+
+    @Test
+    void shouldGetCategoryForPrice() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        List<Product> filterList = productMoving.getCategoryForPrice(Category.BOOK, 250);
+
+        assertEquals(2, filterList.size());
+    }
+
+    @Test
+    void shouldGetCategoryWithDiscount() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        List<Product> filterList = productMoving.getCategoryWithDiscount(Category.BOOK, 10);
+
+        assertEquals(420*0.9, filterList.get(0).getPrice());
+        assertEquals(38*0.9, filterList.get(filterList.size()-1).getPrice());
+    }
+
+    @Test
+    void shouldGetCheapest() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        Product cheapProduct = productMoving.getCheapest();
+
+        assertEquals(2.5, cheapProduct.getPrice());
+    }
+
+    @Test
+    void shouldGetNullCheapest() {
+
+        assertThrows(IllegalStateException.class, () -> productMoving.getCheapest());
+    }
+
+    @Test
+    void shouldGetLatestProducts() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        List<Product> filterList = productMoving.getLatestProducts(3);
+
+        assertEquals(LocalDate.of(2021,12,12), filterList.get(2).getAddingDate());
+    }
+
+    @Test
+    void shouldGetSumByCriterion() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        LocalDate beginYear = LocalDate.of(2020, 1,1);
+        double sum = productMoving.getSumByCriterion(beginYear, Category.BOOK, 500);
+
+        assertEquals(420, sum);
+    }
+
+    // --- для проверки выведем категорию BOOK и соотв. этой категории список объектов Product, их должно быть 4
+    @Test
+    void shouldGetMapList() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        Map<Category, List<Product>> map = productMoving.getMapList();
+
+        assertTrue(map.containsKey(Category.BOOK));
+        assertTrue(map.containsKey(Category.PAINT));
+
+        List<Product> listUnion = map.get(Category.BOOK);
+        assertEquals(4, listUnion.size());
+    }
+
+    // --- для проверки выведем категорию BOOK и соотв. этой категории строку со списком книг через запятую
+    @Test
+    void shouldGetMapString() {
+        for (Product elem : arrayProduct) productMoving.add(elem);      // добавление элементов из массива в список
+
+        Map<Category, String> map = productMoving.getMapString();
+
+        assertTrue(map.containsKey(Category.BOOK));
+        assertEquals(map.get(Category.BOOK), "OOP, Java, Python, Fortran");
+    }
+}
