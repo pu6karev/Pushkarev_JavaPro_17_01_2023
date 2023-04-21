4fd9cadf55b73f147abd67a6ae3109d5f8b7901a
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-04-21 09:50:25 +0300

    add homework 21

---
+++ src/main/java/ua/ithillel/lesson22/Hero.java
@@ -0,0 +1,21 @@
+package ua.ithillel.lesson22;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+
+public class Hero {
+
+    String name;
+    String gender;
+    String eyeColor;
+    String race;
+    String hairColor;
+    double height;
+    String publisher;
+    String skinColor;
+    String alignment;
+    int weight;
+}
