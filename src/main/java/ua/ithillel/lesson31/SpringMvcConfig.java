b2c7149d63d6456c004977cabb9f24e6849f6dfb
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-05-22 13:15:39 +0300

    fix error two main methods

--- src/main/java/ua/ithillel/lesson31/SpringMvcConfig.java
+++ src/main/java/ua/ithillel/lesson31/SpringMvcConfig.java
@@ -1,22 +1,26 @@
 package ua.ithillel.lesson31;
 
+import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ua.ithillel.lesson23.HeroFabric;
 import ua.ithillel.lesson23.HeroServiceDao;
-import ua.ithillel.lesson27.RestAppHero;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringMvcConfig {
+
     @Bean
     public HeroServiceDao heroService(){
         return HeroFabric.createServiceRest(getDataSource());
     }
 
-    @Bean
-    public DataSource getDataSource(){
-        return RestAppHero.getDataSource();
+    private static DataSource getDataSource(){
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setDatabaseName("dbHero");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
     }
 }
