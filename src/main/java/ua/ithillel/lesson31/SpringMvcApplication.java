5f9d71ce190971cfa7caa34ecc0ab1cea9ddfd77
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-05-22 13:25:12 +0300

    fix error two main methods

--- src/main/java/ua/ithillel/lesson31/SpringMvcApplication.java
+++ src/main/java/ua/ithillel/lesson31/SpringMvcApplication.java
@@ -2,8 +2,10 @@ package ua.ithillel.lesson31;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("ua.ithillel.lesson31")
 public class SpringMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcApplication.class, args);
