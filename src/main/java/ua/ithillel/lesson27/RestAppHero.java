7aaf5e9fe1e6c2bad5c09222dc163177784273bc
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-05-22 13:45:07 +0300

    fix error two main methods2

--- src/main/java/ua/ithillel/lesson27/RestAppHero.java
+++ src/main/java/ua/ithillel/lesson27/RestAppHero.java
@@ -4,14 +4,12 @@ import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import ua.ithillel.lesson23.HeroFabric;
 import ua.ithillel.lesson23.HeroServiceDao;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
-@Component
 public class RestAppHero {
     public static void main(String[] args) {
         SpringApplication.run(RestAppHero.class, args);
