0ffe7d39cfe1625c85ed6ff6293ddebb023db23b
Vasiliy Pushkarev <49376973+pu6karev@users.noreply.github.com>
2023-05-24 11:45:01 +0300

    add homework 27 MVC (#29)
    * add homework 27 MVC
    
    * fix two main classes
    
    * fix application.properties

---
+++ src/main/java/ua/ithillel/lesson31/SpringMvcConfig.java
@@ -0,0 +1,27 @@
+package ua.ithillel.lesson31;
+
+
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ua.ithillel.lesson23.HeroFabric;
+import ua.ithillel.lesson23.HeroServiceDao;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringMvcConfig {
+
+    @Bean
+    public HeroServiceDao heroService(){
+        return HeroFabric.createServiceRest(getDataSource());
+    }
+
+    private DataSource getDataSource(){
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setDatabaseName("dbHero");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
+    }
+}
