ffa490d744e290638eafd33d900e69a05539f7d2
Vasiliy Pushkarev <49376973+pu6karev@users.noreply.github.com>
2023-05-27 08:54:38 +0300

    add homework 28 Data JPA (#30)
    * add homework 28 Data JPA
    
    * fix update method

---
+++ src/main/java/ua/ithillel/bank/dataJpa/UserRepository.java
@@ -0,0 +1,8 @@
+package ua.ithillel.bank.dataJpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/ua/ithillel/bank/dataJpa/UserService.java
@@ -0,0 +1,51 @@
+package ua.ithillel.bank.dataJpa;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public User getById(Long id){
+        User user = userRepository.getReferenceById(id);
+        System.out.println("user #" + id + " " + user.toString());
+        return user;
+    }
+
+    @Transactional
+    public User save(User user){
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(Long id, User updatedUser){
+        var user = userRepository.findById(id).orElseThrow();
+        user.setId(updatedUser.getId());
+        userRepository.save(user);
+    }
+
+//    @Transactional
+//    public void update(User updatedUser){
+//        updatedUser.setId(updatedUser.getId());  // чтобы метод save не создавал новую запись
+//        userRepository.save(updatedUser);
+//    }
+
+    @Transactional
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
+}
